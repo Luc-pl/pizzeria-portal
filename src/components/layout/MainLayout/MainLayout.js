cdc76f3f2349e6259da3a3bdccdab979160785d1
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-10-07 00:02:19 +0200

    Add MUI

--- src/components/layout/MainLayout/MainLayout.js
+++ src/components/layout/MainLayout/MainLayout.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import PageNav from '../PageNav/PageNav';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Container from '@mui/material/Container';
 
 const MainLayout = props => (
-  <div>
-    <PageNav />
-    {props.children}
+  <div className="MainLayout">
+    <AppBar>
+      <Container maxWidth='lg'>
+        <Toolbar disableGutters>
+          < PageNav />
+        </Toolbar>
+      </Container>
+    </AppBar>
+    <Container maxWidth='lg'>
+      <Toolbar />    
+      {props.children}
+    </Container>
   </div>
 );
 
