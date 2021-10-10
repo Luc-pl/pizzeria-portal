2cee16455dec4cc26e654140326876f686e1e173
Łukasz Mijała <lukasz.mijala@gmail.com>
2021-10-11 00:26:02 +0200

    Add Tables

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,17 +1,98 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'; 
 import styles from './Login.module.scss';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { Avatar, Button, Checkbox, FormControlLabel, TextField, Typography, Link } from '@mui/material';
+import { LockOutlined } from '@mui/icons-material';
 
-const Login = props => {
-  return (
-    <div className={styles.component}>
-      <h2>Login view</h2>
-    </div>
-  );
-};
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { username: '', password:'', authflag:1 };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-Login.propTypes = {
-  children: PropTypes.node,
-};
+  static propTypes = {
+    history: PropTypes.node,
+  }
+
+  handleChange(event) {
+    this.setState({ username: event.state.username, password: event.state.password });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.history.push('/panel');
+  }
+
+
+  render() {
+
+    return (
+      <Grid>
+        <Paper elevation={5} className={styles.paperStyle}>
+          <Grid align='center' margin-top='20'>
+            <Avatar className={styles.avatarStyle}>
+              <LockOutlined/>
+            </Avatar>
+            <h2>Sign in</h2>
+          </Grid>
+          <TextField 
+            type="email"
+            placeholder="Email"
+            fullWidth
+            name="username"
+            variant="outlined"
+            value={this.state.username}
+            onChange={(event) =>
+              this.setState({
+                [event.target.name]: event.target.value,
+              })
+            }
+            required
+            autoFocus
+          />
+          <TextField 
+            type="password"
+            placeholder="Password"
+            fullWidth
+            name="password"
+            variant="outlined"
+            value={this.state.password}
+            onChange={(event) =>
+              this.setState({
+                [event.target.name]: event.target.value,
+              })
+            }
+            required
+          />
+          <FormControlLabel 
+            control={
+              <Checkbox 
+                name="checkedB"
+                color="primary"
+              />
+            }
+            label="Rember me"
+          />
+          <Button type='submit' color='primary' variant="contained" fullWidth>Sign in</Button>
+          <Typography > 
+            <Link href="#">
+              Forgot password?
+            </Link>
+          </Typography>
+          <Typography > Do you have an account?
+            <Link href="#">
+              Sign up
+            </Link>
+          </Typography>
+        </Paper>
+
+      </Grid>
+    );
+  }
+}
 
 export default Login;
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,9 +1,174 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Tables.module.scss';
 import {Link} from 'react-router-dom';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
 
-const Tables = props => {
+const tableContent = [
+  {
+    hour: '9:00',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'booked', bookingId: '1'},
+      {id: '3', status: 'free', bookingId: null},
+      {id: '4', status: 'booked', bookingId: '2'},
+    ],
+  },
+  {
+    hour: '9:30',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'free', bookingId: null},
+      {id: '3', status: 'booked', bookingId: '3'},
+      {id: '4', status: 'booked', bookingId: '4'},
+    ],
+  },
+  {
+    hour: '10:00',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'free', bookingId: null},
+      {id: '3', status: 'free', bookingId: null},
+      {id: '4', status: 'booked', bookingId: '5'},
+    ],
+  },
+  {
+    hour: '10:30',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'free', bookingId: null},
+      {id: '3', status: 'booked', bookingId: '6'},
+      {id: '4', status: 'free', bookingId: null},
+    ],
+  },
+  {
+    hour: '11:00',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'free', bookingId: null},
+      {id: '3', status: 'booked', bookingId: '8'},
+      {id: '4', status: 'booked', bookingId: '7'},
+    ],
+  },
+  {
+    hour: '11:30',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'free', bookingId: null},
+      {id: '3', status: 'free', bookingId: null},
+      {id: '4', status: 'booked', bookingId: '9'},
+    ],
+  },
+  {
+    hour: '12:00',
+    tables: [
+      {id: '1', status: 'booked', bookingId: '13'},
+      {id: '2', status: 'booked', bookingId: '11'},
+      {id: '3', status: 'booked', bookingId: '12'},
+      {id: '4', status: 'booked', bookingId: '10'},
+    ],
+  },
+  {
+    hour: '12:30',
+    tables: [
+      {id: '1', status: 'booked', bookingId: '16'},
+      {id: '2', status: 'booked', bookingId: '15'},
+      {id: '3', status: 'booked', bookingId: '14'},
+      {id: '4', status: 'booked', bookingId: '17'},
+    ],
+  },
+  {
+    hour: '13:00',
+    tables: [
+      {id: '1', status: 'booked', bookingId: '18'},
+      {id: '2', status: 'booked', bookingId: '19'},
+      {id: '3', status: 'event', bookingId: null},
+      {id: '4', status: 'booked', bookingId: '20'},
+    ],
+  },
+  {
+    hour: '13:30',
+    tables: [
+      {id: '1', status: 'free', bookingId: null},
+      {id: '2', status: 'booked', bookingId: '23'},
+      {id: '3', status: 'booked', bookingId: '21'},
+      {id: '4', status: 'booked', bookingId: '22'},
+    ],
+  },
+  {
+    hour: '14:00',
+    tables: [
+      {id: '1', status: 'booked', bookingId: '24'},
+      {id: '2', status: 'free', bookingId: null},
+      {id: '3', status: 'event', bookingId: null},
+      {id: '4', status: 'event', bookingId: null},
+    ],
+  },
+];
+
+const renderActions = (status, singleTable) => {
+  switch (status) {
+    case 'free':
+      return (
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/table/booking/new`}>Free</Button>
+      );
+    case 'booked':
+      return (
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/table/booking/${singleTable.bookingId}`}>Booked</Button>  
+      );
+    case 'event':
+      return (
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/table/events/${singleTable.bookingId}`}>Event</Button>
+      );
+    default:
+      return null;
+  }
+};
+
+const Tables = () => (
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Hour</TableCell>
+          <TableCell>Table 1</TableCell>
+          <TableCell>Table 2</TableCell>
+          <TableCell>Table 3</TableCell>
+          <TableCell>Table 4</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody> 
+        {tableContent.map((table) => {
+          return (
+            <TableRow component='tr' key={table.hour}>
+              <TableCell className={styles.hour}>{table.hour}</TableCell> 
+              {table.tables.map((singleTable) => {
+                return (
+                  <TableCell 
+                    component='tr'
+                    className={styles.link}
+                    to={`${process.env.PUBLIC_URL}/table/booking/${singleTable.bookingId}`}
+                    key={singleTable.id}>{renderActions(singleTable.status, singleTable)}
+                  </TableCell>
+                );
+              })}    
+            </TableRow> 
+          );
+        })} 
+      </TableBody>
+    </Table>
+  </Paper>
+);
+
+
+
+
+/*const Tables = props => {
   return (
     <div className={styles.component}>
       <h2>Tables view</h2>
@@ -17,6 +182,6 @@ const Tables = props => {
 
 Tables.propTypes = {
   children: PropTypes.node,
-};
+};*/
 
 export default Tables;
